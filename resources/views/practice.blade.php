d4aa30e74ec7cbc39739477e876e47350f31ab1f
Ibnulhaisan <ibnulhaisan1@gmail.com>
2023-05-09 00:34:29 +0600

    practicebladestyle changed

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -23,7 +23,7 @@
 
     <div class="h-75 bg-danger w-75">height 75%</div>
     <div class="h-100 bg-warning w-100">height 100%</div>
-    <div class="h-auto bg-primary w-auto" style="color: white;">height auto</div><br>
+    <div class="h-auto bg-primary w-auto" style="color: white;">height auto</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
